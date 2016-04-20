6b47aca40c944b24ae7d1f95890f6875f3214056
Khurrum Nasim <kny2k@yahoo.com>
2016-04-20 12:44:33 -0400

    Update Contract.java

--- src/InterAbstractorizer/Contract.java
+++ src/InterAbstractorizer/Contract.java
@@ -1,9 +1,5 @@
 package InterAbstractorizer;
 
 	public interface Contract {
-		public static void main(String args []) {
-			System.out.println("hell there");
-		}
-		public String myName(String n);
-		public String myAddress(String a);	
+			
 	}
